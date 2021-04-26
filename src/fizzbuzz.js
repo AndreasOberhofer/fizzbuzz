92c3a02d2d2fe05163fd68b70f5a717011820cf9
AndreasOberhofer <andreas.oberhofer@otto.de>
2021-04-26 13:12:08 +0200

    create the fizzbuzz function just to make the test green

---
+++ src/fizzbuzz.js
@@ -0,0 +1,9 @@
+const fizzbuzz = ( n => {
+    return createData(n) 
+})
+
+const createData = ( n => {
+    return [...Array(n)].map((_, index) => index + 1)
+})
+
+module.exports = fizzbuzz
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,8 +1,9 @@
 const { describe, it, expect } = require('@jest/globals')
+const fizzbuzz = require('./fizzbuzz')
 
 describe('data', () => {
     it('should return an array with 1 to n elements', () => {
-        expect(fizzbuzz(5)).toBe([1, 2, 3, 4, 5]);
+        expect(fizzbuzz(5)).toStrictEqual([ 1, 2, 3, 4, 5 ]);
     })
 })
 
