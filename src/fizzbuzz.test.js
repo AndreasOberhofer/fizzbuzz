9473e3c68c5570da6eca2ab46cbafeab7ce6a943
AndreasOberhofer <andreas.oberhofer@otto.de>
2021-05-03 13:50:30 +0200

    create failing test for buzz and minor changes

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -2,20 +2,25 @@ const { describe, it, expect } = require('@jest/globals');
 const fizzbuzz = require('./fizzbuzz');
 const createData = require('./createdata')
 
-const data = createData(7)
-
 describe('data', () => {
     it('should return an array with a spesific number of elements', () => {
-        expect(data).toHaveLength(7);
+        expect(createData(100)).toHaveLength(100);
     })
 
     it('should return an array with 1 to n elements', () => {
-        expect(data).toEqual([ 1, 2, 3, 4, 5, 6, 7 ]);
+        expect(createData(7)).toEqual([ 1, 2, 3, 4, 5, 6, 7 ]);
     })
 })
 
 describe('functionality', () => {
     it('should change the data, replace the number with fizz when its a multiple of 3', () => {
         expect(fizzbuzz(7)[2]).toEqual("fizz")
+    })
+})
+
+
+describe('functionality', () => {
+    it('should change the data, replace the number with buzz when its a multiple of 5', () => {
+        expect(fizzbuzz(7)[4]).toEqual("buzz")
     })
 })
